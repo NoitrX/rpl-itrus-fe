1aae3ea1dbb177d19e927f29cb19d773dcbd3e14
Mednoob <ahmadyaqdhan123@gmail.com>
2024-07-13 00:13:26 +0900

    feat: change display message

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,7 +5,9 @@ import PostDisplay from "../../components/PostDisplay";
 export default function TestPage() {
     return (
         <div className="w-screen h-screen bg-slate-400 py-36 px-64">
-            <PostDisplay profilePicture="/sample-pfp.png" displayName="Ahmad Yaqdhan" username="Mednoob" caption="Aku wibu." likeCount={19} commentCount={19} shareCount={19} image="/sample-pfp.png"/>
+            <PostDisplay profilePicture="/sample-pfp.png" displayName="Ahmad Yaqdhan" username="Mednoob" caption="IH. ITU ANAK TETANGGA KO GITU BGT. SOMBONG BELI BARANG BARU
+LANGSUNG DI PAMERIN. AKUSIH EWWW BGT YH. JYJYCK SM ORG KEA GTU.
+IHHHHH." likeCount={19} commentCount={19} shareCount={19} image="/sample-pfp.png"/>
         </div>
     )
 }
