f7ee24e46e01e2edf2ee8cdb7f1816bbb0fc4696
Naufhal <naufhalsimdigxrpl2@gmail.com>
2024-07-14 19:51:48 +0700

    a

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,13 +1,22 @@
-"use client"
+"use client";
 
 import PostDisplay from "../../components/PostDisplay";
 
 export default function TestPage() {
-    return (
-        <div className="w-screen h-screen bg-slate-400 py-36 px-64">
-            <PostDisplay profilePicture="/sample-pfp.png" displayName="Ahmad Yaqdhan" username="Mednoob" caption="IH. ITU ANAK TETANGGA KO GITU BGT. SOMBONG BELI BARANG BARU
+  return (
+    <div className="w-screen h-screen bg-slate-400 py-36 px-64">
+      <PostDisplay
+        profilePicture="/sample-pfp.png"
+        displayName="Ahmad Yaqdhan"
+        username="Mednoob"
+        caption="IH. ITU ANAK TETANGGA KO GITU BGT. SOMBONG BELI BARANG BARU
 LANGSUNG DI PAMERIN. AKUSIH EWWW BGT YH. JYJYCK SM ORG KEA GTU.
-IHHHHH." likeCount={19} commentCount={19} shareCount={19} image="/sample-pfp.png"/>
-        </div>
-    )
+IHHHHH."
+        likeCount={19}
+        commentCount={19}
+        shareCount={19}
+        image="/sample-pfp.png"
+      />
+    </div>
+  );
 }
